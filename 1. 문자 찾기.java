e540e4b3348b262d6b8709f3161031de6ab90dc5
jiny <zumod3naranja@gmail.com>
2022-11-21 23:31:28 +0900

    add solution

--- 1. 문자 찾기.java
+++ 1. 문자 찾기.java
@@ -4,7 +4,12 @@ public class Main {
 
   public String solution(String str) {
     String answer = "";
-    
+    for(char x : str.toCharArray()) {
+      if(Character.isLowerCase(x))
+        answer += Character.toUpperCase(x);
+      else
+        answer += Character.toLowerCase(x);
+    }
     return answer;
 }
   
