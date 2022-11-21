914b9b4ad2b85a4b6fd5a547640ae252d7ba29da
jiny <zumod3naranja@gmail.com>
2022-11-21 23:27:14 +0900

    first

---
+++ 1. 문자 찾기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+  
+public class Main {
+
+  public String solution(String str) {
+    
+    return answer;
+}
+  
+  public static void main(String[] args){
+  	Main T = new Main();
+    Scanner sc = new Scanner(System.in);
+    String str = sc.next();
+    System.out.println(T.solution(str));
+  }
+}
