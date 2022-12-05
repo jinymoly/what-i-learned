ce94aa5c7793e45f8f58325c86f0e8cd739f0df9
jiny <zumod3naranja@gmail.com>
2022-12-05 14:40:24 +0900

    피라미드 만들기

---
+++ IllMakeARectangleForYou.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class IllMakeARectangleForYou {
+    
+    public static void main(String[] args) {
+        int row;
+        int column;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("몇 줄 직사각형 만들래? >>>>> ");
+        column = input.nextInt();
+        row = column+1;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        IntStream.range(0, row).forEach(s-> stringBuilder.append("Yes"));
+        IntStream.range(0, column).forEach(s-> 
+        System.out.println(stringBuilder.toString()));
+
+
+    }
+}
---
+++ newPyramidApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+// 피라미드 만들기 
+public class newPyramidApp {
+    public static void main(String[] args) {
+        int line;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("피라미드 몇 층 만들래? >>>> ");
+        line = scanner.nextInt();
+
+        for(int i=line, add=0; i>0; i--, add++){
+            for (int j=0; j<i; j++){
+                System.out.print(" ");
+            } 
+            for (int j=0; j<line-(i-1)+add; j++){
+                System.out.print("&");
+            }
+        
+            System.out.println();
+        }
+    }
+}
