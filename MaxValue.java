f3610a3680934fcc1b7fdc1deb4946ce8c40bb4e
jiny <zumod3naranja@gmail.com>
2022-12-06 23:55:51 +0900

    로직 수정

--- MaxValue.java
+++ MaxValue.java
@@ -5,15 +5,24 @@ public class MaxValue {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        System.out.print("숫자 5개 입력하세요 : >>>>> ");
+
+        System.out.println("----- 숫자 5개 입력 해보렴 -----");
+
         int[] numbers = new int[5];
         
-        
-        for(int i=0; i<=5;i++){
+        for(int i=0; i<numbers.length; i++){
+            System.out.print("numbers["+ i + "] = ");
             numbers[i] = sc.nextInt();
-            System.out.println("num[" + i + "]");
         }
-        sc.close();
+       sc.close();
+
+       int max = numbers[0];
+       for(int i=1; i<numbers.length; i++){
+        if(max<numbers[i]) max = numbers[i];
+       }
+       System.out.println("최댓값은 : " + max + "!!!");
+        
+       
 
         
     }
