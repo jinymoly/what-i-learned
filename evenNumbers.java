620e046162ee2fcb430a56e3e97aae642c6da178
jiny <zumod3naranja@gmail.com>
2022-12-13 12:56:55 +0900

    짝수의 합

---
+++ evenNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class evenNumbers {
+    
+    public static void main(String[] arg) {
+        int answer = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("짝수의 합을 구해드립니다.");
+        System.out.println("n의 범위를 지정하세요 ---- 입력 :");
+        int n = scanner.nextInt();
+        for(int i = 0; i <=n; i++) {
+            if(i%2 == 0){
+                answer +=i; 
+            }
+        }
+        System.out.println(answer + "입니다.");
+        
+    }
+}
