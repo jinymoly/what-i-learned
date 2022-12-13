7a95e4fad53fd2fabc2c0f38e97213661a7158fa
jiny <zumod3naranja@gmail.com>
2022-12-13 22:02:01 +0900

    예외처리 추가

--- Protractor.java
+++ Protractor.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 public class Protractor {
     
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         int answer = 0;
-
+        
         System.out.println("몇 도 ?");
         Scanner scanner = new Scanner(System.in);
         answer = scanner.nextInt();
-        
+
+        if(answer>180){
+            throw new Exception("180도 이하로 다시 설정하세요.");
+        }
         if(0<answer && answer<90){
             System.out.println("1 예각입니다.");
         } else if(answer ==90){
@@ -23,4 +26,4 @@ public class Protractor {
         
     }
 
-    }
+}
