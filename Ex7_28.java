09d26b42c187363a79f996f972b8e5526daa46a9
jiny <zumod3naranja@gmail.com>
2023-01-05 16:18:07 +0900

    예제 7-28 빌드업

--- Ex7_28.java
+++ Ex7_28.java
@@ -1,3 +1,4 @@
+
 public class Ex7_28 {
     
     public static void main(String[] args) {
@@ -5,6 +6,9 @@ public class Ex7_28 {
 
         buyer.buy(new Computer("apple"));
         buyer.buy(new Tv1("yello", "star"));
+        buyer.buy(new Audio());
+        buyer.summery();
+
         System.out.println("현재 잔액은 "+buyer.money + "만원 입니다.");
         System.out.println("현재 적립된 포인트는 " + buyer.bonusPoint + "점 입니다.");
     }
@@ -22,7 +26,6 @@ public class Ex7_28 {
     }
 
     class Tv1 extends Product {
-
         String color;
         String shape;
 
@@ -50,18 +53,49 @@ public class Ex7_28 {
         }
     }
 
+    class Audio extends Product {
+        Audio(){
+            super(50);
+        }
+
+        public String toString() {
+            return "Audio";
+        }
+    }
+
     class Buyer {
         int money = 1000;
         int bonusPoint = 0;
+        Product[] cart = new Product[5];
+        int i = 0;
 
         void buy(Product p){
             if(money < p.price){
                 System.out.println("잔액이 부족하여 물건을 구매할 수 없다.");
             return;
+            }
+        money -= p.price;
+        bonusPoint += p.bonusPrice;
+        cart[i++] = p;
+        System.out.println(">> "+p + " << 를 구입하셨군요.");
         }
 
-        money -=p.price;
-        bonusPoint +=p.bonusPrice;
-        System.out.println(">> "+p + " << 를 구입하셨군요.");
+        void summery(){
+            int sum = 0;
+            String pList = "";
+
+            for(int i = 0; i<cart.length; i++){
+                 if(cart[i]==null) 
+                 break;
+                 sum += cart[i].price;
+                 pList += cart[i]+ ",";
+
+                }
+                System.out.println("지금까지 구입하신 물건의 가격은 총 " + sum + "입니다.");
+                System.out.println("지금까지 구입하신 물건은 " + pList + "입니다.");
+                
+        }
+
+        
+        
     }
-}
