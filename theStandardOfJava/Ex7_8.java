d496daec81f481342fc69d306314bbfb6c9998bd
jiny <zumod3naranja@gmail.com>
2023-01-07 14:26:28 +0900

    연습문제 7-8

--- theStandardOfJava/Ex7_8.java
+++ theStandardOfJava/Ex7_8.java
@@ -1,4 +1,26 @@
 package theStandardOfJava;
+
 public class Ex7_8 {
-    
+    public static void main(String[] args) {
+        Outer5 o = new Outer5();
+        Outer5.Inner inner = o.new Inner();
+
+        inner.method1();
+    }
+}
+
+class Outer5 {
+    int value = 10;
+
+    class Inner {
+        int value = 20;
+
+        void method1() {
+            int value = 30;
+
+            System.out.println(value);
+            System.out.println(this.value);
+            System.out.println(Outer5.this.value);
+        }
+    }
 }
