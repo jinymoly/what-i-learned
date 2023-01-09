ca8b59f4a6701b076f871b4ceee01c89cbe4f558
jiny <zumod3naranja@gmail.com>
2023-01-09 13:37:46 +0900

    예외처리 연습문제 8-9

---
+++ theStandardOfJava/Ex8_9.java
@@ -0,0 +1,30 @@
+package theStandardOfJava;
+
+public class Ex8_9 {
+    
+    public static void main(String[] args) {
+        throw new UnsupportedFunctionException("지원하지 않는 기능입니다.", 100);
+    }
+}
+
+class UnsupportedFunctionException extends RuntimeException {
+    
+    private int ERR_CODE = 100;
+
+    UnsupportedFunctionException(String msg, int errCode) {
+        super(msg);
+        ERR_CODE = errCode;
+    }
+
+    UnsupportedFunctionException(String msg){
+        this(msg, 100);
+    }
+
+    public int getErrorCode() {
+        return ERR_CODE;
+    }
+
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
