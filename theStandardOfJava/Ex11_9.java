cf82254ed9971a310384047a0198dfb860881ec8
jiny <zumod3naranja@gmail.com>
2023-01-17 13:08:53 +0900

    HashSet 예제 9부터 12

---
+++ theStandardOfJava/Ex11_9.java
@@ -0,0 +1,24 @@
+package theStandardOfJava;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Ex11_9 {
+    public static void main(String[] args) {
+        Object[] objArr = {"1", new Integer(1), "2", "2", "3", "3", "4", "5", "5"};
+        Set set = new HashSet<>();
+
+        for(int i = 0; i<objArr.length; i++) {
+            set.add(objArr[i]);
+        }
+
+        System.out.println("set : " + set);
+
+        Iterator iterator = set.iterator();
+
+        while(iterator.hasNext()) {
+            System.out.println("next : " + iterator.next());
+        }
+    }
+}
