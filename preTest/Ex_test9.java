b74c76db6bd4edbe6ce22b64bca193bc7f1d9db5
jiny <zumod3naranja@gmail.com>
2023-01-27 22:03:27 +0900

    feat : 추가 숫자만 추출

---
+++ preTest/Ex_test9.java
@@ -0,0 +1,28 @@
+package preTest;
+
+import java.util.Scanner;
+
+/*
+ * 숫자만 추출하기 
+ */
+class Main {
+    
+    public int solution(String str){
+        String answer = "";
+        for(char x : str.toCharArray()){
+            if(Character.isDigit(x))
+                answer +=x;
+        }
+            return Integer.parseInt(answer);
+
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+
+
+    }
+}
