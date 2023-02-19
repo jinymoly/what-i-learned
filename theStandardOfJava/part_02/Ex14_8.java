3f57184a970f3ae07b3b9ad8c5a4b30e1a1cb2dd
jiny <zumod3naranja@gmail.com>
2023-02-19 18:22:18 +0900

    feat : 스트림 예제 14-8

---
+++ theStandardOfJava/part_02/Ex14_8.java
@@ -0,0 +1,35 @@
+package theStandardOfJava.part_02;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public class Ex14_8 {
+    
+    public static void main(String[] args) {
+        Optional<String> optString = Optional.of("banana");
+        Optional<Integer> optInt = optString.map(String::length);
+        System.out.println("optString = " + optString);
+        System.out.println("optInt = " + optInt);
+
+        int result1 = Optional.of("123").filter(x -> x.length() > 0).map(Integer::parseInt).get();
+        int result2 = Optional.of("").filter(x -> x.length() > 0).map(Integer::parseInt).orElse(-99);
+
+        System.out.println("result1 : " + result1);
+        System.out.println("result2 : " + result2);
+
+        Optional.of("456").map(Integer::parseInt).ifPresent(x -> System.out.printf("result3 ~~ %d%n", x));
+
+        OptionalInt optInt1 = OptionalInt.of(0);
+        OptionalInt optInt2 = OptionalInt.empty();
+
+        System.out.println(optInt1.isPresent());
+        System.out.println(optInt2.isPresent());
+
+        System.out.println(optInt1.getAsInt());
+        //System.out.println(optInt2.getAsInt()); -> NoSuchElementException
+        System.out.println("optInt1 : " + optInt1);
+        System.out.println("optInt2 : " + optInt2);
+
+        System.out.println("optInt1.equals(optInt2)? " + optInt1.equals(optInt2));
+    }
+}
