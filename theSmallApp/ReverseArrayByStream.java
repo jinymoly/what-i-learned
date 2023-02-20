59416bc9a5a7fd293d6180379173323598043b1e
jiny <zumod3naranja@gmail.com>
2023-02-20 21:00:36 +0900

    feat : 스트림을 활용한 문자 뒤집기

---
+++ theSmallApp/ReverseArrayByStream.java
@@ -0,0 +1,26 @@
+package theSmallApp;
+
+import java.util.Scanner;
+
+public class ReverseArrayByStream {
+ 
+}
+
+class Main {
+    
+    public String solution(String str){
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+        
+        }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        System.out.print("입력하세요 >>> ");
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+
+
+    }
+}
