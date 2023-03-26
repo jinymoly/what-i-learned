562ed61afb3bd0e18d1630ca70ddd59b9803ee7d
jiny <zumod3naranja@gmail.com>
2023-03-26 23:48:31 +0900

    Refactor : 직각삼각형 출력하기

--- programmers/P_120823.java
+++ programmers/P_120823.java
@@ -26,14 +26,25 @@ public class P_120823 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String s = "*";
 
+        // String s = "*";
+
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j < i + 1; j++){
+        //         System.out.print(s);
+        //     }
+        //     System.out.println();
+        // }
+
+        
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j < i + 1; j++){
-                System.out.print(s);
-            }
-            System.out.println();
+            System.out.println("*".repeat(i));
         }
 
     }
+    
+
+    
+
+    
 }
