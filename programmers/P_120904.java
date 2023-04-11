6e9b935e25ff48b3a1b1a9d39300f75ee7d86d92
jiny <zumod3naranja@gmail.com>
2023-04-12 01:47:30 +0900

    Feat : 숫자 찾기

---
+++ programmers/P_120904.java
@@ -0,0 +1,46 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.List;
+/*
+숫자 찾기
+문제 설명
+정수 num과 k가 매개변수로 주어질 때, num을 이루는 숫자 중에 k가 있으면 num의 그 숫자가 있는 자리 수를 return하고 없으면 -1을 return 하도록 solution 함수를 완성해보세요.
+
+제한사항
+0 < num < 1,000,000
+0 ≤ k < 10
+num에 k가 여러 개 있으면 가장 처음 나타나는 자리를 return 합니다.
+입출력 예
+num	k	result
+29183	1	3
+232443	4	4
+123456	7	-1
+입출력 예 설명
+입출력 예 #1
+
+29183에서 1은 3번째에 있습니다.
+입출력 예 #2
+
+232443에서 4는 4번째에 처음 등장합니다.
+입출력 예 #3
+
+123456에 7은 없으므로 -1을 return 합니다.
+ */
+public class P_120904 {
+    public int solution(int num, int k) {
+        int answer = 0;
+        String[] numArr = String.valueOf(num).split("");
+        List<String> numList = Arrays.asList(numArr);
+
+        for(int i = 0; i < numList.size(); i++){
+            if(numList.contains(String.valueOf(k))){
+                int index = numList.indexOf(String.valueOf(k));
+                answer = index + 1;
+            } else {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
