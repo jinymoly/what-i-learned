39dc740d549a15f10296e787d2eac09443122a5e
jiny <zumod3naranja@gmail.com>
2023-04-22 12:11:03 +0900

    Fix : StringBuilder추가 및 hashMap for문 내 조건문 파라미터 변경

--- programmers/P_120838.java
+++ programmers/P_120838.java
@@ -51,44 +51,45 @@ a ~ z에 해당하는 모스부호가 순서대로 담긴 배열입니다.
 public class P_120838 {
     public String solution(String letter){
         String answer = "";
-        String[] splitLetter =  letter.split("");
-        StringBuilder sb = new StringBuilder();
+        String[] splitLetter =  letter.split(" ");
         Map<String, String> morse = new HashMap<>();
-        morse.put(".-","a");
-        morse.put("-...","b");
-        morse.put("-.-.","c");
-        morse.put("-..","d");
-        morse.put(".","e");
-        morse.put("..-.","f");
-        morse.put("--.","g");
-        morse.put("....","h");
-        morse.put("..","i");
-        morse.put(".---","j");
-        morse.put("-.-","k");
-        morse.put(".-..","l");
-        morse.put("--","m");
-        morse.put("-.","n");
-        morse.put("---","o");
-        morse.put(".--.","p");
-        morse.put("--.-","q");
-        morse.put(".-.","r");
-        morse.put("...","s");
-        morse.put("-","t");
-        morse.put("..-","u");
-        morse.put("...-","v");
-        morse.put(".--","w");
-        morse.put("-..-","x");
-        morse.put("-.--","y");
-        morse.put("--..","z");
-
+            morse.put(".-","a");
+            morse.put("-...","b");
+            morse.put("-.-.","c");
+            morse.put("-..","d");
+            morse.put(".","e");
+            morse.put("..-.","f");
+            morse.put("--.","g");
+            morse.put("....","h");
+            morse.put("..","i");
+            morse.put(".---","j");
+            morse.put("-.-","k");
+            morse.put(".-..","l");
+            morse.put("--","m");
+            morse.put("-.","n");
+            morse.put("---","o");
+            morse.put(".--.","p");
+            morse.put("--.-","q");
+            morse.put(".-.","r");
+            morse.put("...","s");
+            morse.put("-","t");
+            morse.put("..-","u");
+            morse.put("...-","v");
+            morse.put(".--","w");
+            morse.put("-..-","x");
+            morse.put("-.--","y");
+            morse.put("--..","z");
+        StringBuilder sb = new StringBuilder();
+        
         for(int i = 0; i < splitLetter.length; i++){
-            for(String s : morse.keySet()){
-                if(splitLetter[i].equals(morse.get(s))){
-                    sb.append(s);
+            for(String key : morse.keySet()){
+                if(splitLetter[i].equals(key)){
+                    sb.append(morse.get(key));
                 }
             }
         }
         answer = sb.toString();
+    
         return answer;
     }
 }
