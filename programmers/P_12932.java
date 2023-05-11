1ed83db589741c8e63381806192eec06a066bbb4
jiny <zumod3naranja@gmail.com>
2023-05-11 23:51:35 +0900

    Feat : 자연수 뒤집어 배열로 만들기

---
+++ programmers/P_12932.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+자연수 뒤집어 배열로 만들기
+문제 설명
+자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+제한 조건
+n은 10,000,000,000이하인 자연수입니다.
+입출력 예
+n	return
+12345	[5,4,3,2,1]
+ */
+public class P_12932 {
+    public int[] solution(long n) {
+        int nInt = (int)n;
+
+        ArrayList aList = new ArrayList(nInt);
+        Collections.reverse(aList);
+
+        int[] answer = new int[aList.size()];
+        return answer;
+        
+    }
+}
