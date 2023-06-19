d1dd9e9b1b59487a440e257b7841d92a7d693be3
jiny <zumod3naranja@gmail.com>
2023-06-19 23:47:18 +0900

    Feat : 배열로 풀기

--- programmers/java/P_181916.java
+++ programmers/java/P_181916.java
@@ -1,4 +1,7 @@
 package programmers.java;
+
+import java.util.Arrays;
+
 /*
 주사위 게임 3
 문제 설명
@@ -97,5 +100,32 @@ public class P_181916 {
         if(num < 0)
             return -num;
         return num;
+    }
+
+    // 배열 풀이 >>
+    public int solution2(int a, int b, int c, int d) {
+
+        int[] dice = { a, b, c, d };
+        Arrays.sort(dice);
+
+        int ans = 0;
+
+        if (dice[0] == dice[3]) {
+            ans = 1111 * dice[3];
+        } else if (dice[0] == dice[2] || dice[1] == dice[3]) {
+            ans = (int) Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]), 2);
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]) {
+            ans = (dice[0] + dice[3]) * (dice[3] - dice[0]);
+        } else if (dice[0] == dice[1]) {
+            ans = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]) {
+            ans = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]) {
+            ans = dice[0] * dice[1];
+        } else {
+            ans = dice[0];
+        }
+
+        return ans;
     }
 }
