26b2e67eb1e875670fe22f6950b41ceae491a4ab
jiny <zumod3naranja@gmail.com>
2023-07-10 23:54:13 +0900

    Feat : 조건에 맞게 수열 변환하기 1(2)

--- programmers/java/P_181882.java
+++ programmers/java/P_181882.java
@@ -19,8 +19,17 @@ arr	result
  */
 public class P_181882 {
     public int[] solution(int[] arr) {
-        int[] answer = {};
+        int[] answer = new int[arr.length];
 
+        for(int i = 0; i < answer.length; i++){
+            if( arr[i] >= 50 && arr[i] % 2 == 0){
+                answer[i] = arr[i] / 2;
+            } else if(arr[i] < 50 && arr[i] % 2 == 1) {
+                answer[i] = arr[i] * 2;
+            } else {
+                answer[i] = arr[i];
+            }
+        }
         return answer;
     }
 }
