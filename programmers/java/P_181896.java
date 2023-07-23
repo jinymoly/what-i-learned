6c6005d0c5f5405d989cf2cb675ee7a04c7c0ad8
jiny <zumod3naranja@gmail.com>
2023-07-23 23:46:07 +0900

    Feat : 첫 번째로 나오는 음수

--- programmers/java/P_181896.java
+++ programmers/java/P_181896.java
@@ -38,4 +38,13 @@ public class P_181896 {
             return 0;
         }
     }
+    
+    public int solution2(int[] num_list){
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] < 0){
+                return i;
+            }
+        }
+        return -1;
+    }
 }
