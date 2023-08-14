3739ded078a7657f62cf0cc670f1d9a7d3b93d03
jiny <zumod3naranja@gmail.com>
2023-08-14 23:51:59 +0900

    Feat : 카운트 다운

---
+++ programmers/java/P_181899.java
@@ -0,0 +1,27 @@
+package programmers.java;
+/*
+카운트 다운
+문제 설명
+정수 start와 end가 주어질 때, start에서 end까지 1씩 감소하는 수들을 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+제한사항
+0 ≤ end ≤ start ≤ 50
+입출력 예
+start	end	result
+10	3	[10, 9, 8, 7, 6, 5, 4, 3]
+입출력 예 설명
+입출력 예 #1
+
+10부터 3까지 1씩 감소하는 수를 담은 리스트는 [10, 9, 8, 7, 6, 5, 4, 3]입니다.
+ */
+public class P_181899 {
+    public int[] solution(int start, int end){
+        int size = start - end + 1;
+        int[] answer = new int[size];
+
+        for(int i = 0; i < size; i++){
+            answer[i] = start - i;
+        }
+        return answer;
+    }
+}
