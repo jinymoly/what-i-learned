7a9f7ab65a3042c4a54801d55bd2ef89a874757c
jiny <zumod3naranja@gmail.com>
2023-10-22 23:39:22 +0900

    Fix : Rising Temperature

--- leetCode/sql/L_197.sql
+++ leetCode/sql/L_197.sql
@@ -45,13 +45,28 @@ Explanation:
 In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 */
+-- 오답 쿼리
 select id 
-from
-    (select id, temperature, 
-            lag(temperature) over(order by recordDate) as prev_temperature
-    from weather)
-where temperature > prev_temperature;
+from weather
+where temperature > (
+        select case
+            when temperature > lag(temperature) over (order by recordDate) then temperature
+            else lag(temperature) over(order by recordDate)
+        end as max_temp
+        from weather
+)
 
 -- LAG() : 윈도우 함수 중 하나, 데이터 비교, 누적 및 통계 연산과 같은 분석적인 작업을 수행하는 데 유용
 -- 현재 행의 이전 행(직전 행)의 값을 검색
 -- OVER 절과 함께 사용되며, 특정 열을 기준으로 정렬된 결과 집합에서 이전 행의 값을 가져온다.
+
+-- "single-row subquery returns more than one row" 오류는 하위 쿼리(subquery)가 여러 행을 반환하고, 
+-- 메인 쿼리에서는 단일 값을 예상할 때 발생 
+-- 위 쿼리의 경우, LAG 함수를 사용한 하위 쿼리가 여러 행을 반환하여 발생한 문제임 
+
+-- 정답 쿼리
+-- 그냥 단순하게 셀프 조인하여 where절로 날짜차이만 조건으로 걸어줘도 됐던 문제였다.. 
+select w.id as id
+from weather w, weather w2
+where (w.recordDate - w2.recordDate) = 1
+and w.temperature > w2.temperature
