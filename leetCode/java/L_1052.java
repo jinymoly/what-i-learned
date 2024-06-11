b6dbe6a92b0de2fbdf0bc0323aba8ffa367c105d
jiny <zumod3naranja@gmail.com>
2024-06-11 23:57:52 +0900

    Comment : Grumpy Bookstore Owner (Feat.Sliding Window)

--- leetCode/java/L_1052.java
+++ leetCode/java/L_1052.java
@@ -33,36 +33,44 @@ n == customers.length == grumpy.length
 1 <= minutes <= n <= 2 * 104
 0 <= customers[i] <= 1000
 grumpy[i] is either 0 or 1.
+
+[슬라이딩 윈도우]
+고정 사이즈의 윈도우가 이동하면서 윈도우 내에 있는 데이터를 이용해 문제를 풀이하는 알고리즘
+배열이나 리스트의 요소의 일정 범위의 값을 비교
+
+e.g. 구간 합 구하기, 부분 문자열 구하기
+
  */
 public class L_1052 {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int customer = customers.length;
-        int totalSatisfied = 0;
+        int totalSatisfied = 0; 
 
         for(int i = 0; i < customer; i++){
-            if(grumpy[i] == 0){
-                totalSatisfied += customers[i];
+            if(grumpy[i] == 0){ // 서점 주인이 심술 부리지 않으면 
+                totalSatisfied += customers[i]; // 초기, 비밀 기술 없이 만족한 고객을 증가 
             }
         }
-        int maxSatisfied = 0;
-        int currentSatisfied = 0;
+        
+        int maxSatisfied = 0; // 비밀 기술 사용 시 최대로 만족할 수 있는 고객 수 
+        int currentSatisfied = 0;  // 현재 슬라이딩 윈도우 내 추가로 만족 고객 수 
 
-        for(int i = 0; i < minutes; i++){
-            if(grumpy[i] == 1){
-                currentSatisfied += customers[i];
+        for(int i = 0; i < minutes; i++){ // 슬라이딩 윈도우 - 첫 minutes 구간 만족 고객 수 계산 
+            if(grumpy[i] == 1){ // 심술 on 
+                currentSatisfied += customers[i]; // 비밀 기술 적용이므로 불만족이어도 만족으로 침
             }
         }
         maxSatisfied = currentSatisfied;
         
-        for(int i = minutes; i < customer; i++){
-            if(grumpy[i] == 1){
-                currentSatisfied += customers[i];
+        for(int i = minutes; i < customer; i++){ // 슬라이딩 윈도우 - 나머지 구간 만족 고객 수 계산
+            if(grumpy[i] == 1){ // 심술 on
+                currentSatisfied += customers[i]; // 비밀 기술 적용이므로 불만족이어도 만족으로 침
             }
-            if(grumpy[i - minutes] == 1){
-                currentSatisfied -= customers[i - minutes];
+            if(grumpy[i - minutes] == 1){ // 윈도우에서 벗어난 시간에 서점 주인이 심술 on
+                currentSatisfied -= customers[i - minutes]; // 이 때는 불만족으로 침 
             }
-            maxSatisfied = Math.max(maxSatisfied, currentSatisfied);
+            maxSatisfied = Math.max(maxSatisfied, currentSatisfied); // 현재 슬라이딩 윈도우 에서 추가 만족 고객 수 vs 기존 최대값
         }
-        return totalSatisfied + maxSatisfied;
+        return totalSatisfied + maxSatisfied; // 걍 만족 + 비밀 기술 사용 시 만족한 최대 고객 수 
     }
 }
